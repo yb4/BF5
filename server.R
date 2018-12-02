225dddaba6b63184e4229e839ed9c83ad22a7cd7
Yash Baldawa <yashb4@D-10-19-80-29.dhcp4.washington.edu>
2018-12-02 03:27:12 -0800

    Tried making some changes

--- server.R
+++ server.R
@@ -4,19 +4,42 @@ library(dplyr)
 library(data.table)
 library(ggplot2)
 library(R.utils)
-
+setwd("~/Desktop/BF5/")
 homeless_by_state <- data.table :: fread("data/Homelessness.csv", sep = ",", stringsAsFactors = FALSE)
 total_homeless <- homeless_by_state %>% filter(Measures == "Total Homeless")
 write.csv(total_homeless, "data/total_homeless.csv", row.names = TRUE)
 
 # Help needed here, trying to calculate the sum total of the Homeless Population
 # gives me the nrow() instead
-sum_total_homeless <- sum(total_homeless, sum = sum(Count))
+#sum_total_homeless <- sum(total_homeless, sum = sum(Count))
 
-
+homeless_per_state <- filter(total_homeless, State =="CA") 
+                      
 
 server <- function(input, output) {
-  output$hm_per_state <- renderPlot({
-    
-  })
-}
+  output$country_wide_graph <- renderPlotly({
+  library(plotly)
+  df$hover <- with(df, paste(State, '<br>', "County", CoC Name,"<br>",
+                             "Count", Count))
+  # give state boundaries a white border
+  l <- list(color = toRGB("White"), width = 2)
+  # specify some map projection/options
+  g <- list(
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showlakes = TRUE,
+    lakecolor = toRGB('blue')
+  )
+  
+  p <- plot_geo(df, locationmode = 'USA-states') %>%
+    add_trace(
+      z = ~Count, text = ~hover, locations = ~State,
+      color = ~Count, colors = 'Purples'
+    ) %>%
+    colorbar(title = "Population") %>%
+    layout(
+      title = 'Distribution of Homeless Populaiton Around the United States<br>(Hover for breakdown)',
+      geo = g
+    )
+})
+  }
