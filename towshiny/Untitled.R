c69a8ff4210cab1a308bceeaf93b1606a5e5f35c
tow mokaramanee <17towm@gmail.com>
2018-12-04 12:30:56 -0800

    finally finished initial map

--- towshiny/Untitled.R
+++ towshiny/Untitled.R
@@ -30,10 +30,10 @@ df_homeless <- left_join(df_homeless, df_pop, by = "State")
 df_homeless <- mutate(df_homeless, Homeless_perc = (Total_homeless / Total_pop) * 100 )
 
 # cleanup before joining sp
-df_homeless <- rbind(df_homeless, data.frame('State' = 'Puerto Rico', 'Total_homeless' = '', 
-                                             'Total_pop' = '', 'Homeless_perc' = ''))
+df_homeless <- rbind(df_homeless, data.frame('State' = 'Puerto Rico', 'Total_homeless' = 0, 
+                                             'Total_pop' = 0, 'Homeless_perc' = 0))
 colnames(df_homeless)[1] <- "name"
-df_homeless[c(2,3,4)] <- sapply(df_homeless[c(2,3,4)],as.numeric)
+df_homeless[c(2,3,4)] <- sapply(df_homeless[c(2,3,4)],as.double)
 
 
 m_geom1 <- 
@@ -42,25 +42,43 @@ m_geom1 <-
 
 m_geom1@data <- right_join(m_geom1@data, df_homeless, by = 'name')
 
-m1 = leaflet(m_geom, width = "100%")
+v_lab = sprintf(stringr::str_c("<strong>State:</strong> %s<br>",
+                                "<strong>Total Population:</strong> %s<br>",
+                                "<strong>Homeless Population: </strong> %s",
+                                collapse = ""),
+                 m_geom1@data$name, m_geom1@data$Total_pop, m_geom1@data$Total_homeless) 
+                # formatC(sp_rent@data$w_median, format = "f", digits = 0, big.mark = ","))
+
+v_lab = purrr::map(v_lab, htmltools::HTML)
+
+f_palet = colorQuantile("RdYlBu", domain = m_geom1@data$Homeless_perc, 
+                       n = 10, reverse = TRUE)
+
+l_hl_options = highlightOptions(weight = 5, color = "#666", dashArray = "",
+                                fillOpacity = 0.4, bringToFront = TRUE)
+
+l_lb_options = labelOptions(style = list("font-weight" = "normal"), 
+                            textsize = "12px")
+
+m1 = leaflet(m_geom1, width = "100%")
 m1 = addTiles(m1)
 
 m1 = addPolygons(m1,
-                fillColor = ~f_pale(Homeless_perc),
+                fillColor = ~f_palet(Homeless_perc),
                 weight = 2,
                 opacity = 1,
                 color = "white",
                 dashArray = "3",
                 fillOpacity = 0.4,
                 highlight = l_hl_options,
-                label = "",
+                label = v_lab,
                 labelOptions = l_lb_options)
 
 m1 = addLegend(m1, 
               position = "bottomright",
-              pal = f_pale,
-              values = sp_rent@data$w_median, 
-              title = "Rent percentile")
+              pal = f_palet,
+              values = m_geom1@data$Homeless_perc, 
+              title = "Homelessness Percentile")
 
 
 
@@ -117,11 +135,7 @@ v_labs = sprintf(stringr::str_c("<strong>County:</strong> %s<br>",
 v_labs = purrr::map(v_labs, htmltools::HTML)
 
 # set options -------------------------------------------------------------
-l_hl_options = highlightOptions(weight = 5, color = "#666", dashArray = "",
-                                fillOpacity = 0.4, bringToFront = TRUE)
 
-l_lb_options = labelOptions(style = list("font-weight" = "normal"), 
-                            textsize = "12px")
 
 # build map ---------------------------------------------------------------
 m = leaflet(sp_rent, width = "100%")
