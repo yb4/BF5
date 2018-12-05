4b2772f78439c81f8c7c38172d5ae9b5d2b043e0
tow mokaramanee <17towm@gmail.com>
2018-12-05 04:19:01 -0800

    main server file used

--- ui.R
+++ ui.R
@@ -1,56 +1,23 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
-library(ggplot2)
-library(
-  
-)
+library(leaflet)
+
 ui <- navbarPage(
-  
-  # App title ----
-  "Homelessness vs Causes",
-  tabPanel("Potential Cause vs Effect per state",
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Select the random distribution type ----
-      selectInput("state_choose", "Choose a State",
-                  choices = list("Alabama" = "AL","Alaska" = "AK",
-                                 "Arizona" = "AZ", "Arkansas" = "AR", 
-                                 "California" = "CA", "Colorado"= "CO", "Connecticut" = "CT",
-                                 "Delaware" = "DE", "Flodrida" = "FL", "Georgia" = "GA",
-                                 "Hawaii" = "HI", "Idaho" = "ID", "Illnois" = "IL",
-                                 "Indiana" = "IN", "Iowa" = "IA", "Kansas" = "KS",
-                                 "Kentucky" = "KY","Louisiana" = "LA", "Maine" = "ME",
-                                 "Maryland" = "MD", "Massachusetts" = "MA", 
-                                 "Michigan" = "MI", "Minnesota" = "MN", "Mississippi" = "MS",
-                                 "Missouri" = "MO", "Montana" = "MT", "Nebraska" = "NE",
-                                 "Nevada" = "NV", "New Hampshire" = "NH", "New Jersey" = "NJ",
-                                 "New Mexico" = "NM","New York" = "NY", "North Carolina" = "NC",
-                                 "North Dakota" = "ND", "Ohio" = "OH", "Oklahoma" = "OK",
-                                 "Oregon" = "OR", "Pennsylvania" = "PA", "Rhode Island" = "RI",
-                                 "South Carolina" = "SC", "South Dakota" = "SD", 
-                                 "Tennessee" = "TN","Texas" = "TX", "Utah" = "UT",
-                                 "Vermont" = "VT", "Virginia" = "VA", "Washington" = "WA",
-                                 "West Virginia" = "WV", "Wisconsin" = "WI", "Wyoming" = "WY", 
-                                 "Washington DC" = "DC"),
-                  selected = "WA"
-      ),
-      
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Tabset w/ plot, summary, and table ----
-      tabsetPanel(type = "tabs",
-                  tabPanel("HomelessNess Per State", plotOutput("HS_state")),
-                  tabPanel("HS_vs_Drugs_and_drinking", plotOutput("HS_D_and_D")),
-                  tabPanel("HS_vs_Increase_in_wage_rent", plotOutput("HS_vs_Wage")),
-                  tabPanel("HomelessNess_per_state", plotOutput("HS_text_per_state"))
-      )
-      
-    )
-  )
-  )
-)
-)
+  "Homelessness in the US",
+  tabPanel("Homelessness Map",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput("indicator", " Indicator", c("Total Homeless", "Chronically Homeless Individuals" ,
+                                                        'Unsheltered Chronically Homeless Individuals')),
+               sliderInput("year", "Year of Interest", 2007, 2016, 2016, step = 1, round = True, ticks = FALSE, sep = "")),
+             mainPanel(leafletOutput("homeless_m"))
+           )
+  ))
