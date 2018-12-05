fdbf2a8e32e77d0ff7968c7f7a4f47f358ca2478
tow mokaramanee <17towm@gmail.com>
2018-12-05 04:15:11 -0800

    new server

--- server.R
+++ server.R
@@ -1,85 +1,82 @@
-#Setting up libraries required for this task
 library(shiny)
+library(leaflet)
 library(dplyr)
-library(data.table)
-library(ggplot2)
-library(R.utils)
-library(ggvis)
-install.packages('openintro')
-library(openintro)
-library(ggrepel)
-setwd("~/Desktop/BF5/")
+library(lubridate)
+library(geojsonio)
 
-homeless_by_state <- data.table :: fread("data/Homelessness.csv", sep = ",", stringsAsFactors = FALSE)
-total_homeless <- homeless_by_state %>% filter(Measures == "Total Homeless") %>% filter(Year == "1/1/2012") 
-write.csv(total_homeless, "data/total_homeless.csv", row.names = TRUE)
-
-#Clean up data for Binge Drinking in States
-drunkards_by_state <- data.table :: fread ("data/binge_drinking.csv", sep = ",", stringsAsFactors = FALSE)
-total_drunkards <- drunkards_by_state %>% select(state, location, both_sexes_2012)
-total_drunkards
-state_drink_csv <- function(state_name) {
-  output_data <- paste("Data/Data_by_states/", state_name, "d.csv", sep = "")
-  state_of_data <- filter(total_drunkards, state == state_name)
-  final_output <- write.csv(state_of_data, output_data)
-  return(final_output)
-}
-lapply(total_drunkards$state,state_drink_csv)
-
-homeless_by_state$Count <- as.numeric(gsub(",","",homeless_by_state$Count))
-homeless_people_per_state <- homeless_by_state%>% filter(Year == "1/1/2012") %>% 
-                            select(State, Count) %>% filter(State != "GU" & State != "PR" & State != "VI") %>% 
-                                filter(Count != "NA") %>%  group_by(State) %>% 
-                          summarise(Count = sum(as.numeric(Count))) %>% mutate(State = tolower(State))
-
-
-# names(homeless_people_per_state) <- c("region", "average")
-# states <- map_data("state")
-# graph <- ggplot(states) + geom_map(data = states, map = states,
-#                                    aes(x = long, y = lat, map_id = region),
-#                                    fill = "#ffffff", color = "#ffffff", size = 0.15)
-# graph <- graph + geom_map(data = homeless_people_per_state, map = states, aes(fill = average, map_id = region))
-
-
-drunk_people_total_by_state <- data.table :: fread ("data/binge_drinking.csv", sep = ",", stringsAsFactors = FALSE)
-drunk_people_per_state <- drunk_people_total_by_state %>% select(state, both_sexes_2012) %>% 
-                           group_by(state) %>% summarise(average = mean(both_sexes_2012)) %>% 
-                          filter(state != "National") 
-drunk_people_per_state <- drunk_people_per_state %>% mutate(state, State = state2abbr(state)) %>% mutate(State = tolower(State)) %>% 
-                          select(State, average)
-
-homeless_and_drunk_people <- left_join(homeless_people_per_state,drunk_people_per_state)
-#names(drunk_people_per_state) <- c("region", "average")
-names(drunk_people_per_state) <- c("State", "average")
-names(homeless_people_per_state) <- c("State", "Count")
-
-scatter_homeless_and_drunk <- ggplot(homeless_and_drunk_people, aes(x=average, y=Count,color = State, label=State)) + 
-                            geom_point(shape = "diamond", size = 1) + theme_minimal() 
-scatter_homeless_and_drunk <- scatter_homeless_and_drunk + geom_label_repel(aes(label=State),
-                              box.padding = 0.35,point.padding = 0.5,
-                             segment.color = 'grey50') + theme_classic() + labs(x = "Average Drinking",
-                            y = "Count of Homeless",title = "Value of Binge Drinking vs Homeless Count for Each State for 2012") 
-#scatter_homeless_and_drunk <- scatter_homeless_and_drunk + theme(axis.text.x = element_blank())
-
-plott <- ggplot (drunk_people_per_state, aes(x=State, y=average), color = "class") + geom_bar(stat = "identity")
-          + ggplot(homeless_people_per_state, aes(x=drunk_people_per_state$state.abb)) 
-
-# Following is the code for a GG Vis Plot for Drunk Peopel and HomeLess People
-# plott <- homeless_people_per_state %>% ggvis(~State, ~Count) %>% layer_points(fill = ~temp_name) #%>% drunk_people_per_state %>% ggvis(~region, ~average) %>% layer_lines()
-# plott <- drunk_people_per_state %>% ggvis(~State, ~average) %>% layer_paths()
-
-names(drunk_people_per_state) <- c("State", "Average")
-states <- map_data("state")
-graph_drunk <- ggplot(states) + geom_map(data = states, map = states,
-                                   aes(x = long, y = lat, map_id = region),
-                                   fill = "grey", color = "white", size = 0.15)
-graph_drunk <- graph_drunk + geom_map(data = drunk_people_per_state, map = states, aes(fill = Average, map_id = State))
-
-# Clean up data for homeless people in States
-state_save_csv <- function(state_name) {
-  output_data <- paste("Data/Data_by_states/", state_name, ".csv", sep = "")
-  state_of_data <- filter(total_homeless, State == state_name)
-  final_output <- write.csv(state_of_data, output_data, row.names = FALSE)
-  return(final_output)
-}
-lapply(total_homeless$State,state_save_csv)
+shinyServer(function(input, output) {
+  # load homeless data
+  df_homeless <- read.csv("~/Desktop/201/BF5/Data/Homelessness.csv", stringsAsFactors = FALSE)
+  
+  # clean up homeless data
+  state_name <- append(state.name, "District of Columbia", after = 8) 
+  state_abb <- append(state.abb, "DC", after = 8)
+  df_homeless['State'] <- state_name[match(df_homeless$State, state_abb)]
+  df_homeless$Count[is.na(df_homeless$Count)] <- 0
+  df_homeless$Count <- as.numeric(sub(",", "", df_homeless$Count, fixed = TRUE))
+  df_homeless$Year <- dmy(df_homeless$Year)
+  df_homeless <- na.omit(df_homeless)
+  
+  geo_homeless <- geojson_read( x = "~/Desktop/201/BF5/Data/us-states.json"
+                                , what = "sp", stringsAsFactor = FALSE)
+  
+  # render rankng table
+  
+  # render homeless map ---------------------------------------------------------------------
+  output$homeless_m <- renderLeaflet({
+    
+    # filter year and sum
+    df_homeless <- filter(df_homeless, year(Year) == input$year)
+    df_homeless <- filter(df_homeless, Measures == input$indicator) %>% group_by(State) %>% 
+      summarise("total" = sum(Count))
+    
+    # join with population by state
+    df_pop <- read.csv("~/Desktop/201/BF5/Data/acs2015_census_tract_data.csv", stringsAsFactors = FALSE)
+    df_pop <- df_pop %>% group_by(State) %>% summarise("population" = sum(TotalPop))
+    df_homeless <- left_join(df_homeless, df_pop, by = "State")
+    
+    # mutate proportion column
+    df_homeless <- mutate(df_homeless, percentage = (total / population) * 100 )
+    
+    # cleanup before joining sp
+    
+    colnames(df_homeless)[1] <- "name"
+    df_homeless[c(2,3,4)] <- sapply(df_homeless[c(2,3,4)],as.double)
+    
+    # import and join us-state geo sp
+    
+    geo_homeless@data <- right_join(geo_homeless@data, df_homeless, by = 'name')
+    
+    #set label
+    v_lab = sprintf(stringr::str_c("<strong>State:</strong> %s<br>",
+                                   "<strong>Total Population:</strong> %s<br>",
+                                   "<strong>Homeless Population: </strong> %s <br>",
+                                   "<strong>Homeless Percentage: </strong> %.2f%%",
+                                   collapse = ""),
+                    geo_homeless@data$name, geo_homeless@data$population, geo_homeless@data$total,
+                    geo_homeless@data$percentage) 
+    
+    v_lab = purrr::map(v_lab, htmltools::HTML)
+    
+    #set color palet 
+    
+    bins <- seq(0, 0.5, by= 0.05)
+    f_palet = colorBin("YlOrRd", domain = geo_homeless@data$percentage, bins = bins)
+    
+    #Set label and highight options
+    l_hl_options = highlightOptions(weight = 5, color = "#666", dashArray = "",
+                                    fillOpacity = 0.4, bringToFront = TRUE)
+    
+    l_lb_options = labelOptions(style = list("font-weight" = "normal"), 
+                                textsize = "12px")
+    
+    #create map
+    m = leaflet(geo_homeless, width = "100%") %>% 
+      addTiles() %>% 
+      addPolygons(fillColor = ~f_palet(percentage), weight = 2, opacity = 1,color = "white", dashArray = "3",  
+                  fillOpacity = 0.4, highlight = l_hl_options, label = v_lab, labelOptions = l_lb_options) %>% 
+      addLegend(position = "bottomright", pal = f_palet, values = geo_homeless@data$percentage, 
+                title = "Homelessness Percentage")
+  })
+  
+})
--- ui.R
+++ ui.R
@@ -1,100 +1,14 @@
-#In this part of the project, we build the UI to present the widgets and the 
-#visualiztions expected in this assignment
-
 library(shiny)
-library(ggplot2)
-<<<<<<< HEAD
+library(leaflet)
 
-ui <- fluidPage(
-  titlePanel <- ("UFO SIGHTINGS DATA"),
-  tabsetPanel(
-    tabPanel("Homeless Data By American States",
-             fluid = TRUE,
-             sidebarLayout(
-               sidebarPanel(
-                 selectInput("state_select", "Choose a State",
-                             choices = list("Alabama" = "Alabama","Alaska" = "AK",
-                                            "Arizona" = "AZ", "Arkansas" = "AR", 
-                                            "California" = "CA", "Colorado"= "CO", "Connecticut" = "CT",
-                                            "Delaware" = "DE", "Flodrida" = "FL", "Georgia" = "GA",
-                                            "Hawaii" = "HI", "Idaho" = "ID", "Illnois" = "IL",
-                                            "Indiana" = "IN", "Iowa" = "IA", "Kansas" = "KS",
-                                            "Kentucky" = "KY","Louisiana" = "LA", "Maine" = "ME",
-                                            "Maryland" = "MD", "Massachusetts" = "MA", 
-                                            "Michigan" = "MI", "Minnesota" = "MN", "Mississippi" = "MS",
-                                            "Missouri" = "MO", "Montana" = "MT", "Nebraska" = "NE",
-                                            "Nevada" = "NV", "New Hampshire" = "NH", "New Jersey" = "NJ",
-                                            "New Mexico" = "NM","New York" = "NY", "North Carolina" = "NC",
-                                            "North Dakota" = "ND", "Ohio" = "OH", "Oklahoma" = "OK",
-                                            "Oregon" = "OR", "Pennsylvania" = "PA", "Rhode Island" = "RI",
-                                            "South Carolina" = "SC", "South Dakota" = "SD", 
-                                            "Tennessee" = "TN","Texas" = "TX", "Utah" = "UT",
-                                            "Vermont" = "VT", "Virginia" = "VA", "Washington" = "WA",
-                                            "West Virginia" = "WV", "Wisconsin" = "WI", "Wyoming" = "WY", 
-                                            "Washington DC" = "DC"),
-                             selected = "WA"
-                 )
-               ),
-               
-               # This is the main panel, displaying the visualization presenting most commonly occuring shapes in that part
-               # of US and displaying beneath it, the total UFO Observations recorded in that state
-               mainPanel(
-                 plotOutput("states")
-               )
-             ))
-=======
-library(
-  
-)
 ui <- navbarPage(
-  
-  # App title ----
-  "Homelessness vs Causes",
-  tabPanel("Potential Cause vs Effect per state",
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Select the random distribution type ----
-      selectInput("state_choose", "Choose a State",
-                  choices = list("Alabama" = "AL","Alaska" = "AK",
-                                 "Arizona" = "AZ", "Arkansas" = "AR", 
-                                 "California" = "CA", "Colorado"= "CO", "Connecticut" = "CT",
-                                 "Delaware" = "DE", "Flodrida" = "FL", "Georgia" = "GA",
-                                 "Hawaii" = "HI", "Idaho" = "ID", "Illnois" = "IL",
-                                 "Indiana" = "IN", "Iowa" = "IA", "Kansas" = "KS",
-                                 "Kentucky" = "KY","Louisiana" = "LA", "Maine" = "ME",
-                                 "Maryland" = "MD", "Massachusetts" = "MA", 
-                                 "Michigan" = "MI", "Minnesota" = "MN", "Mississippi" = "MS",
-                                 "Missouri" = "MO", "Montana" = "MT", "Nebraska" = "NE",
-                                 "Nevada" = "NV", "New Hampshire" = "NH", "New Jersey" = "NJ",
-                                 "New Mexico" = "NM","New York" = "NY", "North Carolina" = "NC",
-                                 "North Dakota" = "ND", "Ohio" = "OH", "Oklahoma" = "OK",
-                                 "Oregon" = "OR", "Pennsylvania" = "PA", "Rhode Island" = "RI",
-                                 "South Carolina" = "SC", "South Dakota" = "SD", 
-                                 "Tennessee" = "TN","Texas" = "TX", "Utah" = "UT",
-                                 "Vermont" = "VT", "Virginia" = "VA", "Washington" = "WA",
-                                 "West Virginia" = "WV", "Wisconsin" = "WI", "Wyoming" = "WY", 
-                                 "Washington DC" = "DC"),
-                  selected = "WA"
-      ),
-      
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Tabset w/ plot, summary, and table ----
-      tabsetPanel(type = "tabs",
-                  tabPanel("HomelessNess Per State", plotOutput("HS_state")),
-                  tabPanel("HS_vs_Drugs_and_drinking", plotOutput("HS_D_and_D")),
-                  tabPanel("HS_vs_Increase_in_wage_rent", plotOutput("HS_vs_Wage")),
-                  tabPanel("HomelessNess_per_state", plotOutput("HS_text_per_state"))
-      )
-      
-    )
-  )
->>>>>>> master
-  )
-)
-
+  "Homelessness in the US",
+  tabPanel("Homelessness Map",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput("indicator", " Indicator", c("Total Homeless", "Chronically Homeless Individuals" ,
+                                                        'Unsheltered Chronically Homeless Individuals')),
+               sliderInput("year", "Year of Interest", 2007, 2016, 2016, step = 1, round = True, ticks = FALSE, sep = "")),
+             mainPanel(leafletOutput("homeless_m"))
+           )
+  ))
